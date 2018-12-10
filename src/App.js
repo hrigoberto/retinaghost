899ec43a1cfcdf6f157ca378a010fba6a76049df
jfkbeatkid <hernadezrigoberto@gmail.com>
2018-12-09 22:43:17 -0800

    website styled, still need to add social media links, maybe about section

--- src/App.js
+++ src/App.js
@@ -1,28 +1,20 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Gallery from 'react-photo-gallery';
+import PHOTO_SET from './photoSet';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+const App = () => {
+  return (
+    <div>
+      <div className="ui container" style={{ background: "black", margin: "60px"}}>
+        <div className="ui segment" style={{background: "black"}}>
+          <h1 className="ui center aligned header" style={{color : "white"}}>
+            RETINA GHOST
+          </h1>
+        </div>
+        <Gallery photos={PHOTO_SET} direction="column" margin="3"/>
       </div>
-    );
-  }
+    </div>
+  )
 }
 
 export default App;
---
+++ src/photoSet.js
@@ -0,0 +1,136 @@
+const PHOTO_SET = [
+  {
+    src: 'https://s3-us-west-2.amazonaws.com/retinaghost/goldenveintweak.png',
+    width: 2.35,
+    height: 1
+  },
+  {
+    src: 'https://s3-us-west-2.amazonaws.com/retinaghost/conjaboxVerticalfin.png',
+    width: 9,
+    height: 16
+  },
+  {
+    src: 'https://s3-us-west-2.amazonaws.com/retinaghost/JasonWeberMostTweakable.png',
+    width: 16,
+    height: 9
+  },
+  {
+    src: 'https://s3-us-west-2.amazonaws.com/retinaghost/machinehub.png',
+    width: 2.35,
+    height: 1
+  },
+  {
+    src: 'https://s3-us-west-2.amazonaws.com/retinaghost/satchall.png',
+    width: 16,
+    height: 9
+  },
+  {
+    src: 'https://s3-us-west-2.amazonaws.com/retinaghost/fridaychallenge.png',
+    width: 16,
+    height: 9
+  },
+  {
+    src: 'https://s3-us-west-2.amazonaws.com/retinaghost/bluepinkbubble.png',
+    width: 2.35,
+    height: 1
+  },
+  {
+    src: 'https://s3-us-west-2.amazonaws.com/retinaghost/firingmalaserFinished.png',
+    width: 16,
+    height: 9
+  },
+  {
+    src: 'https://s3-us-west-2.amazonaws.com/retinaghost/conjaboxTunnelcelshaded.png',
+    width: 16,
+    height: 9
+  },
+  {
+    src: 'https://s3-us-west-2.amazonaws.com/retinaghost/conjaboxkocksurf.png',
+    width: 1,
+    height: 1
+  },
+  {
+    src: 'https://s3-us-west-2.amazonaws.com/retinaghost/majoraboxmask.png',
+    width: 16,
+    height: 9
+  },
+  {
+    src: 'https://s3-us-west-2.amazonaws.com/retinaghost/conjaboxTWEAKMOREFinished.png',
+    width: 16,
+    height: 9
+  },
+  {
+    src: 'https://s3-us-west-2.amazonaws.com/retinaghost/crabkingdom.png',
+    width: 1,
+    height: 1
+  },
+  {
+    src: 'https://s3-us-west-2.amazonaws.com/retinaghost/dotmatrix.png',
+    width: 1,
+    height: 1
+  },
+  {
+    src: 'https://s3-us-west-2.amazonaws.com/retinaghost/datacenterflythrough.png',
+    width: 9,
+    height: 16
+  },
+  {
+    src: 'https://s3-us-west-2.amazonaws.com/retinaghost/townsquare.png',
+    width: 1,
+    height: 1
+  },
+  {
+    src: 'https://s3-us-west-2.amazonaws.com/retinaghost/townentrance.png',
+    width: 1,
+    height: 1
+  },
+  {
+    src: 'https://s3-us-west-2.amazonaws.com/retinaghost/squigglybigglies.png',
+    width: 1,
+    height: 1
+  },
+  {
+    src: 'https://s3-us-west-2.amazonaws.com/retinaghost/bluewhitedatacenter.png',
+    width: 1,
+    height: 1
+  },
+  {
+    src: 'https://s3-us-west-2.amazonaws.com/retinaghost/datacenterextractionpoint.png',
+    width: 1,
+    height: 1
+  },
+  {
+    src: 'https://s3-us-west-2.amazonaws.com/retinaghost/datacentercross.png',
+    width: 1,
+    height: 1
+  },
+  {
+    src: 'https://s3-us-west-2.amazonaws.com/retinaghost/purpstar.png',
+    width: 1,
+    height: 1
+  },
+  {
+    src: 'https://s3-us-west-2.amazonaws.com/retinaghost/quadpyramid.png',
+    width: 1,
+    height: 1
+  },
+  {
+    src: 'https://s3-us-west-2.amazonaws.com/retinaghost/enterthemainframe.png',
+    width: 1,
+    height: 1
+  },
+  {
+    src: 'https://s3-us-west-2.amazonaws.com/retinaghost/datacenteruploadpoint.png',
+    width: 1,
+    height: 1
+  },
+  {
+    src: 'https://s3-us-west-2.amazonaws.com/retinaghost/spaceport.png',
+    width: 1,
+    height: 1
+  },
+
+
+];
+
+export default PHOTO_SET;
